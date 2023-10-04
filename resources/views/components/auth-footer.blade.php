7b65ab90bb05e9f5382b24c8657849d926991d57
abdrrahmenz <abdrrahmenz@gmail.com>
2023-10-04 11:34:38 +0700

    fic8-task-3

---
+++ resources/views/components/auth-footer.blade.php
@@ -0,0 +1,3 @@
+  <div class="simple-footer">
+      Copyright &copy; Abdurrahman 2023
+  </div>
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('pages.auth.auth-login');
+});
+
+Route::middleware(['auth'])->group(function(){
+    Route::get('home', function () {
+        return view('pages.app.dashboard-siakad',['type_menu' => '']);
+    })->name('home');
+});
